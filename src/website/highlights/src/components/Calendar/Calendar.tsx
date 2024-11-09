847cef2ccce637301f4c42173f1e107469b0495d
Hasintha <aashi.chandu5088@gmail.com>
2024-11-09 13:23:37 +0530

    set the full calendar with library component

--- src/website/highlights/src/components/Calendar/Calendar.tsx
+++ src/website/highlights/src/components/Calendar/Calendar.tsx
@@ -1,50 +1,46 @@
-// components/Calendar/Calendar.tsx
-
 import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import { Container, Title, Button, useMantineTheme } from '@mantine/core';
+import { Container, Title } from '@mantine/core';
 import styles from './Calendar.module.css';
+import './Calendar.module.css';
 
 const MyCalendar: React.FC = () => {
-    const theme = useMantineTheme();
+  const handleDateClick = (arg: any) => {
+    alert(`Date clicked: ${arg.dateStr}`);
+  };
 
-    const handleDateClick = (arg: any) => {
-        alert(`Date clicked: ${arg.dateStr}`);
-    };
+  const handleEventClick = (arg: any) => {
+    alert(`Event clicked: ${arg.event.title}`);
+  };
 
-    const handleEventClick = (arg: any) => {
-        alert(`Event clicked: ${arg.event.title}`);
-    };
-
-    return (
-      <>
-        <Title order={2} className={styles.title}>My Calendar</Title>
-        <Container className={styles.calendarContainer}>
-          <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            initialView="dayGridMonth"
-            editable={true}
-            selectable={true}
-            dateClick={handleDateClick}
-            eventClick={handleEventClick}
-            events={[
-              { title: 'Event 1', date: '2024-11-01' },
-              { title: 'Event 2', date: '2024-11-15' },
-            ]}
-            headerToolbar={{
-              left: 'prev,next today',
-              center: 'title',
-              right: 'dayGridMonth,timeGridWeek,timeGridDay'
-            }}
-            height="auto"
-          />
-          {/* <Button className={styles.addButton}>Add Event</Button>  */}
-        </Container>
-      </>
-    );
+  return (
+    <>
+      <Title order={1} className={styles.title}>My Calendar</Title>
+      <Container className={styles.calendarContainer}>
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          editable={false}
+          selectable={true}
+          dateClick={handleDateClick}
+          eventClick={handleEventClick}
+          events={[
+            { title: 'Event 1', date: '2024-11-01' },
+            { title: 'Event 2', date: '2024-11-15' },
+          ]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay',
+          }}
+          height="auto"
+        />
+      </Container>
+    </>
+  );
 };
 
 export default MyCalendar;
--- src/website/highlights/src/pages/calendar/index.tsx
+++ src/website/highlights/src/pages/calendar/index.tsx
@@ -1,17 +1,16 @@
 import PageLayout from "@/components/PageLayout/PageLayout";
 import { ReactNode } from "react";
-import DaySchedule from "@/components/DaySchedule/DaySchedule";
 import MyCalendar from "@/components/Calendar/Calendar";
-import styles from "./index.module.css"; // Import CSS module styles
+import style from "./index.module.css"; 
 
 export default function Calendar() {
     return (
-        <div className={styles.calendarContainer}>
+        <div className={style.calendarContainer}>
             {/* <div className={styles.leftPlane}>
                 <DaySchedule />
             </div> */}
-            <div className={styles.middleBorder}></div> {/* Middle border element */}
-            <div className={styles.rightPlane}>
+            <div className={style.middleBorder}></div> {/* Middle border element */}
+            <div className={style.rightPlane}>
                 <MyCalendar />
             </div>
         </div>
