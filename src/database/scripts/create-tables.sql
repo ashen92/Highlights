f3aba38d42abcc853f80c6d6c950745a375dd141
jagath <jagathdammika17595@gmail.com>
2024-12-03 06:46:37 +0530

    add new tbls

--- src/database/scripts/create-tables.sql
+++ src/database/scripts/create-tables.sql
@@ -18,6 +18,9 @@ DROP TABLE IF EXISTS `User`;
 DROP TABLE IF EXISTS `Review`;
 DROP TABLE IF EXISTS `LinkedAccount`;
 DROP TABLE IF EXISTS `Project`;
+DROP TABLE IF EXISTS `Projects`;
+DROP TABLE IF EXISTS `assignees`;
+DROP TABLE IF EXISTS `taskss`;
 
 CREATE TABLE `Project` (
 	`id` INT AUTO_INCREMENT,
@@ -195,7 +198,7 @@ CREATE TABLE assignees (
     `taskId` INT NOT NULL,
     `assignee` VARCHAR(255) NOT NULL,
 	`userId` INT NOT NULL,
-    PRIMARY KEY (taskId, assignee)
+    PRIMARY KEY (`taskId`, `assignee`)
 );
 
 
